44c47c0625d96a645bb1c948fc68767de53a4a55
Anand <manand881@gmail.com>
2019-04-02 19:10:30 +0530

    02-04-2019

---
+++ plot/input_param_reader3.py
@@ -0,0 +1,21 @@
+def ising_input():
+
+    nrows = 25              #  number of rows of spins (even number)
+    
+    ncols = 25              #  number of columns of spins (even number)
+
+    nlayers = 3             #  number of layers in the quasi 3D Matrix
+
+    npass = 15500           #  number of passes for each temperature
+
+    nequil = 13000          #  number of equilibration steps for each temperature
+
+    high_temp = 4.0         #  temperature to start scan at
+
+    low_temp = 0.92         #  temperature to finish scan at
+
+    temp_interval = 0.01    #  scanning interval
+
+    ConfigType = 1          #  1: checkerboard, 2: interface, 3: unequal interface, 4: Random Matrix
+
+    return nrows, ncols, nlayers, npass, nequil, high_temp, low_temp, temp_interval, ConfigType
